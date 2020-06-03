3606c0743f18f0f95c442ba7a52756730fe2a1fc
Jordy <jordyvazgol@gmail.com>
2020-06-03 00:27:06 -0600

    first commit

---
+++ AimHigh/AimHigh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AimHigh
+//
+//  Created by Jordy Vazquez on 6/1/20.
+//  Copyright © 2020 Jordy Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
