92f209d359481f8f852b4d5a502918cb41242bec
Jordy <jordyvazgol@gmail.com>
2020-06-08 01:18:29 -0600

    Added color themes and worked on the view controllers layout

---
+++ AimHigh/AimHigh/Views/GoalsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalsTableViewCell.swift
+//  AimHigh
+//
+//  Created by Jordy Vazquez on 6/8/20.
+//  Copyright © 2020 Jordy Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class GoalsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
